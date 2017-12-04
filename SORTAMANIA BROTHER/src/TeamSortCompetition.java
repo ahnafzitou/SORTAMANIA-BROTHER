2e2968791cc5aba55a29bb60c1142e108ece25eb
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2017-12-04 12:47:05 -0500

    pushy

---
+++ SORTAMANIA BROTHER/src/TeamSortCompetition.java
@@ -0,0 +1,33 @@
+
+public class TeamSortCompetition 
+{
+	public static void main(String args[]) {
+		
+	}
+	public static int challengeOne(int[] arr)
+	{
+		return 0;
+		
+	}
+	public static int challengeTwo(String[] arr)
+	{
+		return 0;
+		
+	}
+	public static int challengeThree(int[] arr)
+	{
+		return 0;
+		
+	}
+	public static int challengeFour(int[][] arr)
+	{
+		return 0;
+		
+	}
+	public static int challengeFive(int[] arr)
+	{
+		return 0;
+		
+	}
+
+}
